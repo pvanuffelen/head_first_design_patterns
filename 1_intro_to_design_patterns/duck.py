6a7c7f3179331237560101c9aa3b98c447e20d72
Pieter van Uffelen <pieteruff@gmail.com>
2024-02-21 16:21:44 +0100

    Create the duck class

---
+++ 1_intro_to_design_patterns/duck.py
@@ -0,0 +1,21 @@
+from abc import abstractmethod, ABC
+
+
+class Duck(ABC):
+
+    def __init__(self):
+        self.fly_behavior = FlyBehavior()
+        self.quack_behavior = QuackBehavior()
+
+    @abstractmethod
+    def display(self):
+        pass
+
+    def perform_fly(self):
+        self.fly_behavior.fly()
+
+    def perform_quack(self):
+        self.quack_behavior.quack()
+
+    def swim(self):
+        print("All ducks float, even decoys!")
