e63b9ba0341b3cc15585001dd07563b156d908b5
Pieter van Uffelen <pieteruff@gmail.com>
2024-02-21 17:17:43 +0100

    Reformat both behavioral classes

--- 1_intro_to_design_patterns/flybehavior.py
+++ 1_intro_to_design_patterns/flybehavior.py
@@ -1,18 +1,24 @@
 from abc import ABCMeta, abstractmethod
 
+
 class FlyBehavior(metaclass=ABCMeta):
     """Class that mimics the interface that all flying behavior classes implement"""
+
     @abstractmethod
     def fly(self):
         """Fly method to be overridden by subclass"""
-       raise NotImplementedError
+        raise NotImplementedError
+
 
 class FlyWithWings(FlyBehavior):
     """Flying behavior implementation for ducks that DO fly"""
+
     def fly(self):
         print("I'm flying")
 
+
 class FlyNoWa(FlyBehavior):
     """Flying behavior implementation for ducks that do NOT fly"""
+
     def fly(self):
         print("I can't fly")
--- 1_intro_to_design_patterns/quackbehavior.py
+++ 1_intro_to_design_patterns/quackbehavior.py
@@ -1,20 +1,25 @@
 from abc import ABCMeta, abstractmethod
 
+
 class QuackBehavior(metaclass=ABCMeta):
     """Class that mimics the quack behavior interface"""
+
     @abstractmethod
     def quack(self):
         raise NotImplementedError
+
 
 class Quack(QuackBehavior):
 
     def quack(self):
         print("Quack")
 
+
 class MuteQuack(QuackBehavior):
 
     def quack(self):
         print("<< Silence >>")
+
 
 class Squeak(QuackBehavior):
 
