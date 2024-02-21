8be4fb880e6f81d1aede362bf341f0b5d0e30421
Pieter van Uffelen <pieteruff@gmail.com>
2024-02-21 20:14:52 +0100

    Create and instance of model duck and change it's flying behavior during runtime

--- 1_intro_to_design_patterns/main.py
+++ 1_intro_to_design_patterns/main.py
@@ -1,10 +1,17 @@
 from mallard_duck import MallardDuck
+from model_duck import ModelDuck
+from fly_behavior import FlyRocketPowered
 
 
 def mini_duck_simulator():
-    mallard = MallardDuck()
-    mallard.perform_quack()
-    mallard.perform_fly()
+    mallard = MallardDuck()  # create the mallard duck object
+    mallard.perform_quack()  # perform the quack method
+    mallard.perform_fly()  # perform the fly method
+
+    model = ModelDuck()  # create the model duck object
+    model.perform_fly()  # perform the fly method of the model duck
+    model.set_fly_behavior(FlyRocketPowered())  # set a new fly behavior for the model duck during run time
+    model.perform_fly()  # perform the newly set fly behavior
 
 
 def main():
