e6677ed0367db4e7e8b4514cb1f109e834c9c723
Pieter van Uffelen <pieteruff@gmail.com>
2024-02-28 19:22:43 +0100

    Create the DisplayElement interface

---
+++ 2_the_observer_pattern/display_element.py
@@ -0,0 +1,7 @@
+from abc import ABCMeta, abstractmethod
+
+class DisplayElement(metaclass=ABCMeta):
+    """Class to act as an interface. Contains just one method, display()"""
+    @abstractmethod
+    def display(self):
+        raise NotImplementedError
