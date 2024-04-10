ffbd39c1a1abc2af20537d5e76a5705a2b3b716f
Pieter van Uffelen <pieteruff@gmail.com>
2024-04-10 16:09:43 +0200

    Make the calculate_heat_index a function instead of method

--- 2_the_observer_pattern/heat_index_display.py
+++ 2_the_observer_pattern/heat_index_display.py
@@ -3,6 +3,19 @@ from display_element import DisplayElement
 from weather_data import WeatherData
 
 
+def calculate_heat_index(temperature: float, humidity: float) -> float:
+    T = temperature
+    RH = humidity
+    heat_index = (16.923 + (0.185212 * T) + (5.37941 * RH) - (0.100254 * T * RH) + (0.00941695 * (T * T)) +
+                  (0.00728898 * (RH * RH)) + (0.000345372 * (T * T * RH)) - (0.000814971 * (T * RH * RH)) +
+                  (0.0000102102 * (T * T * RH * RH)) - (0.000038646 * (T * T * T)) +
+                  (0.0000291583 * (RH * RH * RH)) + (0.00000142721 * (T * T * T * RH)) +
+                  (0.000000197483 * (T * RH * RH * RH)) - (0.0000000218429 * (T * T * T * RH * RH)) +
+                  0.000000000843296 * (T * T * RH * RH * RH)) - 0.0000000000481975 * (T * T * T * RH * RH * RH)
+
+    return heat_index
+
+
 class HeatIndexDisplay(Observer, DisplayElement):
 
     def __init__(self, weather_data: WeatherData):
@@ -13,22 +26,10 @@ class HeatIndexDisplay(Observer, DisplayElement):
         self.pressure: float = 0  # psi
         self.heat_index: float = 0  # degrees fahrenheit
 
-    def calculate_heat_index(self, temperature: float, humidity: float) -> float:
-        T = temperature
-        RH = humidity
-        heat_index = (16.923 + (0.185212 * T) + (5.37941 * RH) - (0.100254 * T * RH) + (0.00941695 * (T * T)) +
-                      (0.00728898 * (RH * RH)) + (0.000345372 * (T * T * RH)) - (0.000814971 * (T * RH * RH)) +
-                      (0.0000102102 * (T * T * RH * RH)) - (0.000038646 * (T * T * T)) +
-                      (0.0000291583 * (RH * RH * RH)) + (0.00000142721 * (T * T * T * RH)) +
-                      (0.000000197483 * (T * RH * RH * RH)) - (0.0000000218429 * (T * T * T * RH * RH)) +
-                      0.000000000843296 * (T * T * RH * RH * RH)) - 0.0000000000481975 * (T * T * T * RH * RH * RH)
-
-        return heat_index
-
     def update(self):
         self.temperature = self.weather_data.get_temperature()
         self.humidity = self.weather_data.get_humidity()
-        self.heat_index = self.calculate_heat_index(self.temperature, self.humidity)
+        self.heat_index = calculate_heat_index(self.temperature, self.humidity)
         self.display()
 
     def display(self):
