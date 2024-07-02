35a65c27048d2c0731c0c496415023fd02d7eed8
Pieter van Uffelen <pieteruff@gmail.com>
2024-07-02 15:49:33 +0200

    Create other three ceiling fan command classes

---
+++ 6_the_command_pattern/4_remote_control_with_undo_state/ceiling_fan_low_command.py
@@ -0,0 +1,22 @@
+from command import Command
+from ceiling_fan import CeilingFan
+
+
+class CeilingFanLowCommand(Command):
+    def __init__(self, ceiling_fan: CeilingFan):
+        self.ceiling_fan = ceiling_fan
+        self.prev_speed: int = self.ceiling_fan.get_speed()
+
+    def execute(self):
+        self.prev_speed = self.ceiling_fan.get_speed()
+        self.ceiling_fan.low()
+
+    def undo(self):
+        if self.prev_speed == CeilingFan.HIGH:
+            self.ceiling_fan.high()
+        elif self.prev_speed == CeilingFan.MEDIUM:
+            self.ceiling_fan.medium()
+        elif self.prev_speed == CeilingFan.LOW:
+            self.ceiling_fan.low()
+        elif self.prev_speed == CeilingFan.OFF:
+            self.ceiling_fan.off()
