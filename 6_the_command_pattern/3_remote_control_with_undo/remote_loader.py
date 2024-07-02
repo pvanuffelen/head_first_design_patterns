1c2a83476b9326a9f318ad273741c909b717db09
Pieter van Uffelen <pieteruff@gmail.com>
2024-07-02 15:09:32 +0200

    Implement the testing of the remote control with undo

--- 6_the_command_pattern/3_remote_control_with_undo/remote_loader.py
+++ 6_the_command_pattern/3_remote_control_with_undo/remote_loader.py
@@ -1,44 +1,25 @@
-from remote_control import RemoteControl
+from remote_control_with_undo import RemoteControlWithUndo
 from light import Light
-from garage_door import GarageDoor
-from stereo import Stereo
 
 from light_on_command import LightOnCommand
 from light_off_command import LightOffCommand
-from garage_door_open_command import GarageDoorOpenCommand
-from stereo_on_with_cd_command import StereoOnWithCDCommand
-from stereo_off_command import StereoOffCommand
 
 
 class RemoteLoader:
-    remote_control = RemoteControl()
+    remote_control = RemoteControlWithUndo()
 
-    # Create all devices in proper locations
     living_room_light = Light("Living Room")
-    kitchen_light = Light("Kitchen")
-    garage_door = GarageDoor()
-    stereo = Stereo("Living Room")
 
-    # All light command objects
     living_room_light_on = LightOnCommand(living_room_light)
     living_room_light_off = LightOffCommand(living_room_light)
-    kitchen_light_on = LightOnCommand(kitchen_light)
-    kitchen_light_off = LightOffCommand(kitchen_light)
 
-    # Stereo on and off
-    stereo_on_with_cd = StereoOnWithCDCommand(stereo)
-    stereo_off = StereoOffCommand(stereo)
-
-    # Load commands to remote slots
     remote_control.set_command(0, living_room_light_on, living_room_light_off)
-    remote_control.set_command(1, kitchen_light_on, kitchen_light_off)
-    remote_control.set_command(2, stereo_on_with_cd, stereo_off)
-
-    print(f"{remote_control.to_string()}")
 
     remote_control.on_button_was_pushed(0)
     remote_control.off_button_was_pushed(0)
-    remote_control.on_button_was_pushed(1)
-    remote_control.off_button_was_pushed(1)
-    remote_control.on_button_was_pushed(2)
-    remote_control.off_button_was_pushed(2)
+    print(f"{remote_control.to_string()}")
+    remote_control.undo_button_was_pushed()
+    remote_control.off_button_was_pushed(0)
+    remote_control.on_button_was_pushed(0)
+    print(f"{remote_control.to_string()}")
+    remote_control.undo_button_was_pushed()
