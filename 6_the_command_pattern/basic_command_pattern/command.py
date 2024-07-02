c0af967904cb51d4cbcbc72fd08b6daaf5215595
Pieter van Uffelen <pieteruff@gmail.com>
2024-07-02 14:46:32 +0200

    Remove old folder

--- 6_the_command_pattern/basic_command_pattern/command.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC
-
-
-class Command(ABC):
-    """The Command Interface"""
-    def execute(self):
-        raise NotImplementedError
--- 6_the_command_pattern/basic_command_pattern/no_command.py
+++
@@ -1,8 +0,0 @@
-from command import Command
-
-
-class NoCommand(Command):
-    """Null  class to avoid having to pass a None type"""
-    def execute(self):
-        pass
-
